a59354b856acc392b60d9a4dd8a2554cfae26727
Giuliano Machado <giufma@gmail.com>
2022-08-24 17:46:21 -0300

    finish base logic, missing sprites

---
+++ js/utils.js
@@ -0,0 +1,37 @@
+function rectCollision(rect1, rect2) {
+    return (
+        (rect1.attackBox.position.x + rect1.attackBox.width) >= rect2.position.x && // left side collision
+        (rect2.position.x + rect2.width) >= rect1.attackBox.position.x && // right side collision
+        (rect1.attackBox.position.y + rect1.attackBox.height) >= rect2.position.y && // top collision
+        (rect2.position.y + rect2.height) >= rect1.attackBox.position.y // bottom collision
+    );
+}
+
+function determineWinner({ player, enemy, timerId }) {
+    clearTimeout(timerId);
+    document.getElementById('display-text').style.display = 'flex';
+
+    if (player.health === enemy.health) {
+        document.getElementById('display-text').innerHTML = 'Tie';
+
+    } else if (player.health > enemy.health) {
+        document.getElementById('display-text').innerHTML = 'Player One Wins';
+    } else if (player.health < enemy.health) {
+        document.getElementById('display-text').innerHTML = 'Player Two Wins';
+    }
+}
+
+let timer = 10;
+let timerId = undefined;
+function decreaseTimer() {
+    timerId = setTimeout(decreaseTimer, 1000);
+    if (timer > 0) {
+        timer--;
+    }
+
+    document.getElementById('timer').innerHTML = timer;
+
+    if (timer === 0) {
+        determineWinner({ player, enemy, timerId });
+    }
+}
