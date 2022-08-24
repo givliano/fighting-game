c5e7baf84a81d49d5201b08e44785b76018c3d7d
Giuliano Machado <giufma@gmail.com>
2022-08-24 17:49:33 -0300

    fix indentation

--- index.js
+++ index.js
@@ -9,175 +9,175 @@ c.fillRect(0, 0, canvas.width, canvas.height);
 const gravity = 0.7;
 
 const background = new Sprite({
-    position: {
-        x: 0,
-        y: 0
-    },
-    imageSrc: './img/background.png'
+  position: {
+    x: 0,
+    y: 0
+  },
+  imageSrc: './img/background.png'
 });
 
 const shop = new Sprite({
-    position: {
-        x: 600,
-        y: 128
-    },
-    imageSrc: './img/shop.png',
-    scale: 2.75,
-    framesMax: 6
+  position: {
+    x: 600,
+    y: 128
+  },
+  imageSrc: './img/shop.png',
+  scale: 2.75,
+  framesMax: 6
 });
 
 const player = new Fighter({
-    position:   {
-        x: 0,
-        y: 0
-    },
-    velocity: {
-        x: 0,
-        y: 10
-    },
-    offset: {
-        x: 0,
-        y: 0
-    }
+  position: {
+    x: 0,
+    y: 0
+  },
+  velocity: {
+    x: 0,
+    y: 10
+  },
+  offset: {
+    x: 0,
+    y: 0
+  }
 });
 
 const enemy = new Fighter({
-    position: {
-        x: 400,
-        y: 100
-    },
-    velocity: {
-        x: 0,
-        y: 0
-    },
-    offset: {
-        x: -50,
-        y: 0
-    },
-    color: 'blue'
+  position: {
+    x: 400,
+    y: 100
+  },
+  velocity: {
+    x: 0,
+    y: 0
+  },
+  offset: {
+    x: -50,
+    y: 0
+  },
+  color: 'blue'
 });
 
 const keys = {
-    a:          { pressed: false },
-    d:          { pressed: false },
-    w:          { pressed: false },
-    ArrowLeft:  { pressed: false },
-    ArrowRight: { pressed: false },
-    ArrowUp:    { pressed: false }
+  a: { pressed: false },
+  d: { pressed: false },
+  w: { pressed: false },
+  ArrowLeft: { pressed: false },
+  ArrowRight: { pressed: false },
+  ArrowUp: { pressed: false }
 }
 
 decreaseTimer()
 
 function animate() {
-    window.requestAnimationFrame(animate);
+  window.requestAnimationFrame(animate);
 
-    c.fillStyle = 'black';
-    c.fillRect(0, 0, canvas.width, canvas.height);
+  c.fillStyle = 'black';
+  c.fillRect(0, 0, canvas.width, canvas.height);
 
-    background.update();
-    shop.update();
-    player.update();
-    enemy.update();
+  background.update();
+  shop.update();
+  player.update();
+  enemy.update();
 
 
+  player.velocity.x = 0;
+  enemy.velocity.x = 0;
+
+  // PLAYER MOVEMENT
+  if (keys.a.pressed && player.lastKey === 'a') {
+    player.velocity.x = -5; // 5 px per frame
+  } else if (keys.d.pressed && player.lastKey === 'd') {
+    player.velocity.x = 5;
+  } else {
     player.velocity.x = 0;
+  }
+
+  // ENEMY MOVEMENT
+  if (keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft') {
+    enemy.velocity.x = -5;
+  } else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
+    enemy.velocity.x = 5;
+  } else {
     enemy.velocity.x = 0;
+  }
 
-    // PLAYER MOVEMENT
-    if (keys.a.pressed && player.lastKey === 'a') {
-        player.velocity.x = -5; // 5 px per frame
-    } else if (keys.d.pressed && player.lastKey === 'd') {
-        player.velocity.x = 5;
-    } else {
-        player.velocity.x = 0;
-    }
+  // detect for colision
+  if (rectCollision(player, enemy) && player.isAttacking) { // attackBox position starts from the top left pixel in the player
+    player.isAttacking = false;
+    enemy.health -= 20;
+    document.getElementById('enemy-health').style.width = enemy.health + '%';
+  }
 
-    // ENEMY MOVEMENT
-    if (keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft') {
-        enemy.velocity.x = -5;
-    } else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
-        enemy.velocity.x = 5;
-    } else {
-        enemy.velocity.x = 0;
-    }
+  if (rectCollision(enemy, player) && enemy.isAttacking) {
+    enemy.isAttacking = false;
+    player.health -= 20;
+    document.getElementById('player-health').style.width = player.health + '%';
+  }
 
-    // detect for colision
-    if (rectCollision(player, enemy) && player.isAttacking) { // attackBox position starts from the top left pixel in the player
-        player.isAttacking = false;
-        enemy.health -= 20;
-        document.getElementById('enemy-health').style.width = enemy.health + '%';
-    }
-
-    if (rectCollision(enemy, player) && enemy.isAttacking) {
-        enemy.isAttacking = false;
-        player.health -= 20;
-        document.getElementById('player-health').style.width = player.health + '%';
-    }
-
-    // end game based on health
-    if (enemy.health <= 0 || player.health <= 0) {
-        determineWinner({ player, enemy, timerId });
-    }
+  // end game based on health
+  if (enemy.health <= 0 || player.health <= 0) {
+    determineWinner({ player, enemy, timerId });
+  }
 }
 
 requestAnimationFrame(animate);
 
 window.addEventListener('keydown', (e) => {
-    switch (e.key) {
-        case 'd':
-            keys.d.pressed = true;
-            player.lastKey = 'd';
-            break;
-        case 'a':
-            keys.a.pressed = true;
-            player.lastKey = 'a';
-            break;
-        case 'w':
-            keys.w.pressed = true;
-            player.velocity.y = -20;
-            break;
-        case ' ':
-            player.attack();
-            break;
+  switch (e.key) {
+    case 'd':
+      keys.d.pressed = true;
+      player.lastKey = 'd';
+      break;
+    case 'a':
+      keys.a.pressed = true;
+      player.lastKey = 'a';
+      break;
+    case 'w':
+      keys.w.pressed = true;
+      player.velocity.y = -20;
+      break;
+    case ' ':
+      player.attack();
+      break;
 
-        case 'ArrowRight':
-            keys.ArrowRight.pressed = true;
-            enemy.lastKey = 'ArrowRight';
-            break;
-        case 'ArrowLeft':
-            keys.ArrowLeft.pressed = true;
-            enemy.lastKey = 'ArrowLeft';
-            break;
-        case 'ArrowUp':
-            keys.ArrowUp.pressed = true;
-            enemy.velocity.y = -20;
-            break;
-        case 'ArrowDown':
-            enemy.attack();
-            break;
-    }
+    case 'ArrowRight':
+      keys.ArrowRight.pressed = true;
+      enemy.lastKey = 'ArrowRight';
+      break;
+    case 'ArrowLeft':
+      keys.ArrowLeft.pressed = true;
+      enemy.lastKey = 'ArrowLeft';
+      break;
+    case 'ArrowUp':
+      keys.ArrowUp.pressed = true;
+      enemy.velocity.y = -20;
+      break;
+    case 'ArrowDown':
+      enemy.attack();
+      break;
+  }
 });
 
 window.addEventListener('keyup', (e) => {
-    switch (e.key) {
-        case 'd':
-            keys.d.pressed = false;
-            break;
-        case 'a':
-            keys.a.pressed = false;
-            break;
-        case 'w':
-            keys.w.pressed = false;
-            break;
+  switch (e.key) {
+    case 'd':
+      keys.d.pressed = false;
+      break;
+    case 'a':
+      keys.a.pressed = false;
+      break;
+    case 'w':
+      keys.w.pressed = false;
+      break;
 
-        case 'ArrowRight':
-            keys.ArrowRight.pressed = false;
-            break;
-        case 'ArrowLeft':
-            keys.ArrowLeft.pressed = false;
-            break;
-        case 'ArrowUp':
-            keys.ArrowUp.pressed = false;
-            break;
-    }
+    case 'ArrowRight':
+      keys.ArrowRight.pressed = false;
+      break;
+    case 'ArrowLeft':
+      keys.ArrowLeft.pressed = false;
+      break;
+    case 'ArrowUp':
+      keys.ArrowUp.pressed = false;
+      break;
+  }
 });
--- js/classes.js
+++ js/classes.js
@@ -1,97 +1,97 @@
 class Sprite extends Sprite {
-    constructor({ position, imageSrc, scale = 1, framesMax = 1 }) {
-        this.position = position;
-        this.width = 50;
-        this.height = 150;
-        this.image = new Image();
-        this.image.src = imageSrc;
-        this.scale = scale;
-        this.framesMax = framesMax;
+  constructor({ position, imageSrc, scale = 1, framesMax = 1 }) {
+    this.position = position;
+    this.width = 50;
+    this.height = 150;
+    this.image = new Image();
+    this.image.src = imageSrc;
+    this.scale = scale;
+    this.framesMax = framesMax;
+    this.framesCurrent = 0;
+    this.framesElapsed = 0;
+    this.framesHold = 5;
+  }
+
+  draw() {
+    c.drawImage(
+      this.image,
+      // crop the sprite, divide image by quantity of sprites
+      this.framesCurrent * (this.image.width / this.framesMax), // x coordinate of crop mark
+      0, // y coordinate of crop mark
+      this.image.width / this.framesMax,
+      this.image.height,
+      this.position.x,
+      this.position.y,
+      (this.image.width / this.framesMax) * this.scale,
+      this.image.height * this.scale
+    );
+  }
+
+  update() {
+    this.draw();
+    this.framesElapsed++;
+
+    if (this.framesElapsed % this.framesHold === 0) { // update the shop sprite every 10 ticks
+      if (this.framesCurrent < this.framesMax - 1) { // subtract 1 to avoid being true for the background img
+        this.framesCurrent++;
+      } else {
         this.framesCurrent = 0;
-        this.framesElapsed = 0;
-        this.framesHold = 5;
+      }
     }
-
-    draw() {
-        c.drawImage(
-            this.image,
-            // crop the sprite, divide image by quantity of sprites
-            this.framesCurrent * (this.image.width / this.framesMax), // x coordinate of crop mark
-            0, // y coordinate of crop mark
-            this.image.width / this.framesMax,
-            this.image.height,
-            this.position.x,
-            this.position.y,
-            (this.image.width / this.framesMax) * this.scale,
-            this.image.height * this.scale
-        );
-    }
-
-    update() {
-        this.draw();
-        this.framesElapsed++;
-
-        if (this.framesElapsed % this.framesHold === 0) { // update the shop sprite every 10 ticks
-            if (this.framesCurrent < this.framesMax - 1) { // subtract 1 to avoid being true for the background img
-                this.framesCurrent++;
-            } else {
-                this.framesCurrent = 0;
-            }
-        }
-    }
+  }
 }
 
 class Fighter {
-    constructor({ position, velocity, color = 'red', offset }) {
-        this.position = position;
-        this.velocity = velocity;
-        this.width = 50;
-        this.height = 150;
-        this.lastKey = undefined;
-        this.attackBox = {
-            position: {
-                x: this.position.x,
-                y: this.position.y
-            },
-            offset,
-            width: 100,
-            height: 50
-        };
-        this.color = color;
-        this.isAttacking = undefined;
-        this.health = 100;
+  constructor({ position, velocity, color = 'red', offset }) {
+    this.position = position;
+    this.velocity = velocity;
+    this.width = 50;
+    this.height = 150;
+    this.lastKey = undefined;
+    this.attackBox = {
+      position: {
+        x: this.position.x,
+        y: this.position.y
+      },
+      offset,
+      width: 100,
+      height: 50
+    };
+    this.color = color;
+    this.isAttacking = undefined;
+    this.health = 100;
+  }
+
+  draw() {
+    c.fillStyle = this.color;
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+
+    // attack box draw
+    if (this.isAttacking) {
+      c.fillStyle = 'green';
+      c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height);
     }
+  }
 
-    draw() {
-        c.fillStyle = this.color;
-        c.fillRect(this.position.x, this.position.y, this.width, this.height);
+  update() {
+    this.draw();
 
-        // attack box draw
-        if (this.isAttacking) {
-            c.fillStyle = 'green';
-            c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height);
-        }
+    this.attackBox.position.x = (this.position.x + this.attackBox.offset.x); // makes the `enemy` attackBox inverted
+    this.attackBox.position.y = this.position.y;
+
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+    this.velocity.y += gravity;
+
+    if ((this.position.y + this.height + this.velocity.y) >= canvas.height - 96) { // makes the fighter stop in the ground
+      this.velocity.y = 0;
+    } else {
+      this.velocity.y += gravity;
     }
+  }
 
-    update() {
-        this.draw();
-
-        this.attackBox.position.x = (this.position.x + this.attackBox.offset.x); // makes the `enemy` attackBox inverted
-        this.attackBox.position.y = this.position.y;
-
-        this.position.x += this.velocity.x;
-        this.position.y += this.velocity.y;
-        this.velocity.y += gravity;
-
-        if ((this.position.y + this.height + this.velocity.y) >= canvas.height - 96) { // makes the fighter stop in the ground
-            this.velocity.y = 0;
-        } else {
-            this.velocity.y += gravity;
-        }
-    }
-
-    attack() {
-        this.isAttacking = true;
-        setTimeout(() => this.isAttacking = false, 100); // reset isAttacking state
-    }
+  attack() {
+    this.isAttacking = true;
+    setTimeout(() => this.isAttacking = false, 100); // reset isAttacking state
+  }
 }
